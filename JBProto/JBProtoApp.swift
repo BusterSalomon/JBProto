14a1be380b497a42bed255ce3e421892be8beff4
BuserSalomon <bustersalomonrasmussen@gmail.com>
2021-12-12 14:54:42 +0100

    Initial Commit

---
+++ JBProto/JBProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JBProtoApp.swift
+//  JBProto
+//
+//  Created by Buster Rasmussen on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct JBProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
